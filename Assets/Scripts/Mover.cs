f3625a4847e1ea06f2b53d21621c2e0fe583780f
Vroop68 <john.r.knoop@gmail.com>
2025-04-12 22:54:15 -0600

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,16 +8,13 @@ public class Mover : MonoBehaviour
     [SerializeField] float zValue = 0f;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-                transform.Translate(xValue,yValue,zValue);
-
+        transform.Translate(xValue,yValue,zValue);
     }
 }
